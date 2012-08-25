4e7f58a7748eb414b3744922cf0b82ac82da63bc
Shane Gibbons <shane.r.gibbons@gmail.com>
2012-08-25 17:22:22 -0400

    generalize bootloader script loading, also include minified jquery

--- src/bookmarklet/boot.js
+++ src/bookmarklet/boot.js
@@ -1,6 +1,10 @@
 (function(){
-  LEDE_SCRIPT=document.createElement('SCRIPT');
-  LEDE_SCRIPT.type='text/javascript';
-  LEDE_SCRIPT.src='http://unburythelede.com/bookmarklet/lede.js';
-  document.getElementsByTagName('head')[0].appendChild(LEDE_SCRIPT)
+  var loader = function(source) {
+    var script = document.createElement('SCRIPT');
+    script.type = 'text/javascript';
+    script.src = source;
+    document.getElementsByTagName('head')[0].appendChild(script)
+  }
+  loader('https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js');
+  loader('http://unburythelede.com/bookmarklet/lede.js');
 })();
