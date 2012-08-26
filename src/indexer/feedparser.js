d217d85cff2f761e44c02dd5ef1eb45114de6991
Ryan Meador <ryan.d.meador@gmail.com>
2012-08-26 15:39:59 -0400

    rework a lot of stuff to work with the new schema

--- src/indexer/feedparser.js
+++ src/indexer/feedparser.js
@@ -22,6 +22,11 @@ function updateSourceMetadata(source, parser, indexTime, updated, done) {
   // TODO handle permalink
   
   updateFields.indexed_at = indexTime;
+  var nextIndexTime = new Date(indexTime);
+  nextIndexTime.setMinutes(nextIndexTime.getMinutes() + source.index_interval);
+  updateFields.next_index_at = nextIndexTime;
+
+  // TODO tweak source.index_interval depending on whether or not we got updates, so that it homes in on the correct update frequency for this feed
 
   if (updated) {
     updateFields.new_content_at = indexTime;
@@ -31,55 +36,99 @@ function updateSourceMetadata(source, parser, indexTime, updated, done) {
   dataLayer.Source.update(source.id, updateFields, done);
 }
 
-function updatePostContents(parser, item, callback) {
+/** updates the existing post with the content if it finds it
+ * @param callback  error or result.  result is the post ID if one was found/updated, or false if none was found
+ */
+function updateExistingPost(post, source, indexTime, callback) {
   // TODO handle multiple versions of a single post
-  dataLayer.PostContent.find({
-    uri: item.getPermalink() // TODO improve this to be a little less draconian... we may want it to match if the trailing slash is omitted, or if the leading http:// is missing, or a whole bunch of things
+  dataLayer.Post.findOne({
+    uri: post.getPermalink() // TODO improve this to be a little less draconian... we may want it to match if the trailing slash is omitted, or if the leading http:// is missing, or a whole bunch of things
   },
   function(err, result) {
     if (err) {
-      callback(new Error("updatePostContents error while finding existing post: " + err.message));
-    } else if (result.length > 0) {
-      callback(null, result[0].id);
+      callback(new Error("Error while finding existing post: " + err.message));
+    } else if (result) { // NOTE this section will not execute until we fix the code in checkForUpdatedPosts() to actually give us updated posts instead of only new ones
+      var updateFields = {};
+
+      if (post.getContents() && result.content != post.getContents()) {
+        updateFields.content = post.getContents();
+      }
+
+      if (post.getDescription() && result.description != post.getDescription()) {
+        updateFields.description = post.getDescription();
+      }
+
+      if (post.getTitle() && result.title != post.getTitle()) {
+        updateFields.title = post.getTitle();
+      }
+
+      if (post.getPermalink() && result.uri != post.getPermalink()) {
+        updateFields.uri = post.getPermalink();
+      }
+
+      if (post.getAuthor() && result.author != post.getAuthor()) {
+        updateFields.author = post.getAuthor();
+      }
+
+      if (result.source_id != source.id) { // TODO maybe we should only change this if it is NULL?  in case the same article appears in multiple feeds
+        updateFields.source_id = source.id;
+      }
+
+      if (!_.isEmpty(updateFields)) {
+        updateFields.indexed_at = indexTime;
+        
+        // TODO if we're going to be tracking edits to articles, we might want to archive the old version in another table
+
+        dataLayer.Post.update(result.id, updateFields, function(err, result) {
+          if (err) {
+            //log.error("updateExistingPost error: " + err.message + "\npost title: " + post.getTitle() + "\nerror details: " + util.inspect(err));
+            callback(err);
+          } else {
+            //console.log("result: " + util.inspect(result));
+            callback(null, result.id);
+          }
+        });
+      } else {
+        callback(null, result.id);
+      }
     } else {
-      dataLayer.PostContent.create({
-        content: item.getContents(),
-        description: item.getDescription(),
-        title: item.getTitle(),
-        uri: item.getPermalink()
-      },
-      function(err, result) {
-        if (err) {
-          //log.error("updatePostContents error: " + err.message + "\nitem title: " + item.getTitle() + "\nerror details: " + util.inspect(err));
-          callback(err);
-        } else {
-          //console.log("result: " + util.inspect(result));
-          callback(null, result.rows[0].id);
-        }
-      });
+      callback(null, false);
     }
   });
 }
 
-// assumes function will never be called with an empty list for updatedPosts
-function updatePosts(source, parser, updatedPosts, done) {
+/**
+ * @param source  the source object from which these posts come
+ * @param updatedPosts  an array of parser objects which represent the new/updated posts.  this array may not be empty
+ * @param done  callback
+*/
+function createOrUpdatePosts(source, indexTime, updatedPosts, done) {
   Step(
     function() {
       var group = this.group();
-      _.each(updatedPosts, function(item) {
+      _.each(updatedPosts, function(post) {
+        // TODO can this be made to use Step's parallel() function for greater efficiency?
         var callback = group(); // instantiate this here because we need to do it synchronously and the function where it is used is asynchronous
 
-        updatePostContents(parser, item, function(err, postContentsId) {
-          if (!err) {
+        updateExistingPost(post, source, indexTime, function(err, postContentsId) {
+          if (err) {
+            callback(err)
+            return;
+          }
+
+          if (postContentsId === false) { // existing post wasn't found
             dataLayer.Post.create({
-              // TODO add more
-              feed_id: source.id,
-              post_content_id: postContentsId,
-              published_at: item.getDate()
+              content: post.getContents(),
+              description: post.getDescription(),
+              title: post.getTitle(),
+              uri: post.getPermalink(),
+              author: post.getAuthor(),
+              published_at: post.getDate(),
+              indexed_at: indexTime
             },
-            callback);
+            callback); // TODO perhaps this callback isn't returning the object we expect as the results... not important since we're not using it right now
           } else {
-            callback(err);
+            callback(null, postContentsId);
           }
         });
       });
@@ -93,13 +142,12 @@ function checkForUpdatedPosts(source, parser, callback) {
 
   Step(
     function readPosts() {
-      dataLayer.PostWithContent.find(
+      dataLayer.Post.find(
         {
-          "uri.in": _.map(parser.getItems(), function(item) { return item.getPermalink(); }),
-          "feed_id": source.id
+          "uri.in": _.map(parser.getItems(), function(item) { return item.getPermalink(); })
         },
         {
-          only: [ 'post_id', 'uri', 'post_content_id', 'feed_id' ],
+          only: [ 'id', 'uri', 'source_id' ],
         },
         this);
     },
@@ -109,14 +157,13 @@ function checkForUpdatedPosts(source, parser, callback) {
         throw err;
       }
       
-      //log.trace("found posts: " + util.inspect(results, false, 3));
-      log.debug("found " + results.length + " existing posts matching URI");
+      log.debug("Found " + results.length + " existing posts matching URIs");
       // TODO we can probably improve this from O(n^2) to O(nlogn) by building
       // a hash of URIs
 
       var updatedItems = _.reject(parser.getItems(), function (item) {
         return _.find(results, function(postWithContent) {
-          // TODO check for updates as well as just new ones
+          // TODO check for updates as well as just new ones.  basing it just on the URI like we're doing currently means updateExistingPost() will always return false
           return postWithContent.uri == item.getPermalink();
         });
       });
@@ -130,7 +177,7 @@ function checkForUpdatedPosts(source, parser, callback) {
 
 function parseFeed(source, xml, done) {
   try {
-    log.debug("parsing source " + source.id);
+    log.debug("Parsing source " + source.id);
 
     var indexTime = new Date();
 
@@ -146,7 +193,7 @@ function parseFeed(source, xml, done) {
             var group = this.group();
 
             if (updatedPosts.length > 0) {
-              updatePosts(source, parser, updatedPosts, group());
+              createOrUpdatePosts(source, indexTime, updatedPosts, group());
             }
 
             updateSourceMetadata(source, parser, indexTime, (updatedPosts.length > 0), group());
