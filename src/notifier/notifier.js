bc0c9b36daa28d95bb4a7ca5d646cdcb86c67007
jhayden <jonathan.hayden@gmail.com>
2012-08-26 01:52:34 -0400

    Adding skeleton to the notifier, include datalayer access and working sendgrid mail

---
+++ src/notifier/notifier.js
@@ -0,0 +1,28 @@
+var email = require('mailer');
+var dataLayer = require('../core/datalayer');
+var sgusername = 'lede';
+var sgpassword = 'Passw0rd!';
+
+var view = {
+  firstname: 'Jon',
+  animal: 'shark'
+};
+
+email.send({
+  host: 'smtp.sendgrid.net',
+  port: '587',
+  domain: 'unburythelede.com',
+  template: 'views/digest.hjs',
+  data: view,
+  to: 'jonathan.hayden@gmail.com',
+  from: 'jon@unburythelede.com',
+  subject: 'Test from my dev enviornment',
+  authentication: 'login',
+  username: sgusername,
+  password: sgpassword
+},
+function(err, res){
+  if(err){
+    console.log(err);
+  }
+});
