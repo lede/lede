9b6f1f7cdd8342aa6a07df135ec0009f70f51030
Ryan Meador <ryan.d.meador@gmail.com>
2012-08-26 01:55:02 -0400

    enforce that the settings file is provided on the command line

--- src/core/settings.js
+++ src/core/settings.js
@@ -1,5 +1,11 @@
 // this module is a magic thing to resolve the correct settings path based on
 // the command line arguments, and perhaps at some point in the future, an
 // environment variable or the user name or somesuch
+
+if (process.argv.length < 3 || process.argv[2] == "") {
+  console.log("Settings file must be first parameter");
+  process.exit(1);
+}
+
 var path = require('path')
 module.exports = require(path.resolve(process.argv[2]))
