bb6ad3902324071033b822d42d6f4eda322b68b1
Justin Fyles <tullfan@gmail.com>
2012-08-26 23:33:51 -0400

    Sorting the data

--- src/web/app/controllers/dashboard.js
+++ src/web/app/controllers/dashboard.js
@@ -1,7 +1,14 @@
 var orm = require("../../../core/datalayer").client;
 var _ = require("underscore");
 
-var total_posts_query = "select count(*), date_trunc('hour', created_at) FROM posts group by date_trunc('hour', created_at) ORDER BY date_trunc('hour', created_at);";
+var total_posts_query = 
+	"with counts AS (
+		select count(*), date_trunc('hour', created_at) as hour FROM posts group by date_trunc('hour', created_at) ORDER BY date_trunc('hour', created_at)
+	), padding AS (
+		select 0 AS count, date_trunc('hour', s.a) as hour from generate_series(now() - interval '14 days', now(), interval '1 hour') as s(a)
+	)
+	SELECT padding.hour, padding.count + COALESCE(counts.count, 0) AS count FROM
+	padding LEFT JOIN counts ON counts.hour = padding.hour";
 
 exports.total_posts = function(req, res) {
 	orm.emit("query", total_posts_query, function(err, result) {
