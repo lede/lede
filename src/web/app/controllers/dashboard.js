8eec4458d59a2c9e5184359f31bece8618f42292
Justin Fyles <tullfan@gmail.com>
2012-08-26 23:09:53 -0400

    Actual data

--- src/web/app/controllers/dashboard.js
+++ src/web/app/controllers/dashboard.js
@@ -1,22 +1,32 @@
+var orm = require("../../../core/datalayer").client;
+var _ = require("underscore");
+
+var total_posts_query = "select count(*), date_trunc('hour', created_at) FROM posts group by date_trunc('hour', created_at);";
+
 exports.total_posts = function(req, res) {
-	res.send (
-	  {
-	  	item: [
-	  		"1",
-	        "5",
-	        "7",
-	        "15"
-	  	],
-	  	settings: {
-	  		axisx: [
-	  			"Aug",
-	  			"Sept"
-	  		],
-	  		axisy: [
-	  			"Bad",
-	  			"Good"
-	  				  		]
-	  	}
-	  }
-	);
+	orm.emit("query", total_posts_query, function(err, result) {
+		var items = _.map(result.rows, function(row) {
+			return row.count;
+		});
+	
+
+		res.send (
+		  {
+		  	item: items,
+		  	settings: {
+		  		axisx: [
+		  			"Aug",
+		  		],
+		  		axisy: [
+		  			"Bad",
+		  			"Good"
+		  		]
+		  	}
+		  }
+		);
+	});
 };
+
+function totalPosts() {
+	this.item 
+}
