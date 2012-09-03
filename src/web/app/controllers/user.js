6f7849371d16e02455977285d5a59c5ca4b0e988
Shane Gibbons <shane.r.gibbons@gmail.com>
2012-09-04 07:30:29 +0800

    Flesh out user api endpoints, registration seems to work, login may be working, whoami/logout are acting a bit buggy, I think due to some misuse of connect middleware for the ensure_user filter

--- src/web/app/controllers/user.js
+++ src/web/app/controllers/user.js
@@ -1,9 +1,23 @@
+var User = require('../../../core/datalayer').User;
+var no_err = require('../helpers/core').no_err;
 
-
+// FIXME: currently insecure login, will let you access any account with a username
+// just for testing
 exports.login = function(req, res) {
-  // TODO: implement
-  res.status = 500;
-  res.send({ result: 'Not Yet Implemented' });
+  if(req.body.user_email) {
+    User.findOne({email: req.body.user_email}, no_err(res, function(user) {
+      if(!user) {
+        res.status = 500;
+        res.send({ result: 'Specified username does not exist' });
+      } else {
+        req.session.user_id = user.id;
+        res.send({ result: 'Loggen in as: ' + req.body.user_email });
+      }
+    }));
+  } else {
+    res.status = 500;
+    res.send({ result: 'User name is required but was not specified' });
+  }
 }
 
 exports.logout = function(req, res) {
@@ -13,17 +27,46 @@ exports.logout = function(req, res) {
   }
 }
 
+// sanity check endpoint, likely just for testing
+// sends back the current active user email
+// NOTE: we don't need to check for req.session.user because this should
+// always be behind an ensure_user middleware filter
 exports.whoami = function(req, res) {
-  if(req.session && req.session.user) {
-    // TODO: implement
-    res.send({ result: req.session.user.email });
+  res.send({ result: req.session.user.email });
+}
+
+// FIXME: hacked up registration that doesn't take or create a password
+// just for testing
+exports.register = function(req, res) {
+
+  // email param is required
+  if(req.body.user_email) {
+
+    // ensure we don't already have a user with that email
+    User.findOne({email:  req.body.user_email}, no_err(res, function(user) {
+
+      // duplicate user, yell
+      if(user) {
+        res.status = 500;
+        res.send({ result: 'Specified user email already exists, not re-registering.' });
+      } else {
+
+        // create the new user
+        User.create({ email: req.body.user_email }, no_err(res, function(created_users) {
+          res.send({ result: 'User created for ' + req.body.user_email });
+        }));
+      }
+    }));
   } else {
-    res.end();
+
+    // yell about missing required email param
+    res.status = 500;
+    res.send({ result: 'An email must be specified to register an account' });
   }
 }
 
-exports.register = function(req, res) {
-  // TODO: implement
-  res.status = 500;
-  res.send({ result: 'Not Yet Implemented' });
-}
+
+
+
+
+
--- src/web/app/middleware/user.js
+++ src/web/app/middleware/user.js
@@ -1,17 +1,23 @@
 var User = require('../../../core/datalayer').User;
-var no_err = require('../helpers/core.js').no_err;
+var no_err = require('../helpers/core').no_err;
+var log = require('../../../core/logger').getLogger("web")
+
 
 function ensure_user(req, res, next) {
+  log.info('Verifying user is authenticated');
   if(req.session.user_id) {
     User.findOne({id: req.session.user_id}, no_err(res, function(user) {
       if(!user) {
-        res.redirect('/user/login');
+        res.status = 500;
+        res.send({ result: 'Invalid user id specified'});
       } else {
         req.session.user = user;
         next();
       }
     }));
   } else {
-    res.redirect('/user/login');
+    log.info('A non-logged in session attempted to access a users-only endpoint');
+    res.status = 500;
+    res.send({ result: 'User id is required but was not specified'});
   }
 }
