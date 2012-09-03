dbe160b33f26a5c927762bd7dece64bca405958f
Ryan Meador <ryan.d.meador@gmail.com>
2012-09-03 15:38:25 -0400

    added migrations for indexes and a bunch of not-null constraints

--- src/db/migrations/20120828013122-add-not-null-and-indexes.js
+++ src/db/migrations/20120828013122-add-not-null-and-indexes.js
@@ -12,7 +12,6 @@ exports.up = function(db, callback) {
       db.changeColumn("sources", "created_at", { notNull: true }, this.parallel());
       db.changeColumn("sources", "updated_at", { notNull: true }, this.parallel());
 
-      db.changeColumn("posts", "content", { notNull: true }, this.parallel());
       db.changeColumn("posts", "uri", { notNull: true, unique: true }, this.parallel());
       db.changeColumn("posts", "created_at", { notNull: true }, this.parallel());
       db.changeColumn("posts", "updated_at", { notNull: true }, this.parallel());
@@ -33,13 +32,16 @@ exports.up = function(db, callback) {
     },
     function (err, result) {
       // add indexes
-      db.addIndex("sources", "url_index", ["url"], this.parallel());
-      db.addIndex("sources", "next_index_at_index", ["next_index_at"], this.parallel());
+      db.addIndex("sources", "sources_url_index", ["url"], this.parallel());
+      db.addIndex("sources", "sources_next_index_at_index", ["next_index_at"], this.parallel());
+      db.addIndex("sources", "sources_created_at_index", ["created_at"], this.parallel());
 
-      db.addIndex("posts", "uri_index", ["uri"], this.parallel());
+      db.addIndex("posts", "posts_uri_index", ["uri"], this.parallel());
+      db.addIndex("posts", "posts_created_at_index", ["created_at"], this.parallel());
 
-      db.addIndex("links", "from_post_id_index", ["from_post_id_index"], this.parallel());
-      db.addIndex("links", "to_post_id_index", ["to_post_id"], this.parallel());
+      db.addIndex("links", "links_from_post_id_index", ["from_post_id"], this.parallel());
+      db.addIndex("links", "links_to_post_id_index", ["to_post_id"], this.parallel());
+      db.addIndex("links", "links_created_at_index", ["created_at"], this.parallel());
     },
     callback
   );
@@ -49,40 +51,42 @@ exports.down = function(db, callback) {
   Step(
     function () {
       // remove not-null and unique constraints
-      db.changeColumn("sources", "url", {}, this.parallel());
-      db.changeColumn("sources", "indexable", {}, this.parallel());
-      db.changeColumn("sources", "index_interval", {}, this.parallel());
-      db.changeColumn("sources", "created_at", {}, this.parallel());
-      db.changeColumn("sources", "updated_at", {}, this.parallel());
+      db.changeColumn("sources", "url", { notNull: false, unique: false }, this.parallel());
+      db.changeColumn("sources", "indexable", { notNull: false, defaultValue: undefined }, this.parallel());
+      db.changeColumn("sources", "index_interval", { notNull: false, defaultValue: undefined }, this.parallel());
+      db.changeColumn("sources", "created_at", { notNull: false }, this.parallel());
+      db.changeColumn("sources", "updated_at", { notNull: false }, this.parallel());
 
-      db.changeColumn("posts", "content", {}, this.parallel());
-      db.changeColumn("posts", "uri", {}, this.parallel());
-      db.changeColumn("posts", "created_at", {}, this.parallel());
-      db.changeColumn("posts", "updated_at", {}, this.parallel());
+      db.changeColumn("posts", "uri", { notNull: false, unique: false }, this.parallel());
+      db.changeColumn("posts", "created_at", { notNull: false }, this.parallel());
+      db.changeColumn("posts", "updated_at", { notNull: false }, this.parallel());
 
-      db.changeColumn("links", "from_post_id", {}, this.parallel());
-      db.changeColumn("links", "to_post_id", {}, this.parallel());
-      db.changeColumn("links", "created_at", {}, this.parallel());
-      db.changeColumn("links", "updated_at", {}, this.parallel());
+      db.changeColumn("links", "from_post_id", { notNull: false }, this.parallel());
+      db.changeColumn("links", "to_post_id", { notNull: false }, this.parallel());
+      db.changeColumn("links", "created_at", { notNull: false }, this.parallel());
+      db.changeColumn("links", "updated_at", { notNull: false }, this.parallel());
 
-      db.changeColumn("ledes", "post_id", {}, this.parallel());
-      db.changeColumn("ledes", "user_id", {}, this.parallel());
-      db.changeColumn("ledes", "created_at", {}, this.parallel());
-      db.changeColumn("ledes", "updated_at", {}, this.parallel());
+      db.changeColumn("ledes", "post_id", { notNull: false }, this.parallel());
+      db.changeColumn("ledes", "user_id", { notNull: false }, this.parallel());
+      db.changeColumn("ledes", "created_at", { notNull: false }, this.parallel());
+      db.changeColumn("ledes", "updated_at", { notNull: false }, this.parallel());
 
-      db.changeColumn("users", "email", {}, this.parallel());
-      db.changeColumn("users", "created_at", {}, this.parallel());
-      db.changeColumn("users", "updated_at", {}, this.parallel());
+      db.changeColumn("users", "email", { notNull: false, unique: false }, this.parallel());
+      db.changeColumn("users", "created_at", { notNull: false }, this.parallel());
+      db.changeColumn("users", "updated_at", { notNull: false }, this.parallel());
     },
     function (err, result) {
       // remove indexes
-      db.removeIndex("sources", "url_index", this.parallel());
-      db.removeIndex("sources", "next_index_at_index", this.parallel());
+      db.removeIndex("sources_url_index", this.parallel());
+      db.removeIndex("sources_next_index_at_index", this.parallel());
+      db.removeIndex("sources_created_at_index", this.parallel());
 
-      db.removeIndex("posts", "uri_index", this.parallel());
+      db.removeIndex("posts_uri_index", this.parallel());
+      db.removeIndex("posts_created_at_index", this.parallel());
 
-      db.removeIndex("links", "from_post_id_index", this.parallel());
-      db.removeIndex("links", "to_post_id_index", this.parallel());
+      db.removeIndex("links_from_post_id_index", this.parallel());
+      db.removeIndex("links_to_post_id_index", this.parallel());
+      db.removeIndex("links_created_at_index", this.parallel());
     },
     callback
   );
