a3ab82f4a922de4d466ae4a835642599f5c12f6b
Shane Gibbons <shane.r.gibbons@gmail.com>
2012-09-08 08:58:24 +0800

    move user api endpoints behind /api/

--- src/web/app/app.js
+++ src/web/app/app.js
@@ -68,10 +68,10 @@ app.get('/dashboard/total_posts/:days', posts_dashboard.total_posts_by_day);
 app.get('/dashboard/total_sources', sources_dashboard.total_sources);
 app.get('/dashboard/total_sources/:days', sources_dashboard.total_sources_by_day);
 
-app.post('/user/login', user.login);
-app.post('/user/logout', ensure_user, user.logout);
-app.get('/user/whoami', ensure_user, user.whoami);
-app.put('/user/register', user.register);
+app.post('/api/user/login', user.login);
+app.post('/api/user/logout', ensure_user, user.logout);
+app.get('/api/user/whoami', ensure_user, user.whoami);
+app.put('/api/user/register', user.register);
 
 http.createServer(app).listen(app.get('port'), function(){
   log.info("Express server listening on port " + app.get('port'));
