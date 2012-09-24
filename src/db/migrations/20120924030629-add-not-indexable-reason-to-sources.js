a1a8fe94138b8e2cb9c0de162b51be53b0871f31
Ryan Meador <ryan.d.meador@gmail.com>
2012-09-24 00:56:07 -0400

    add not indexable reason column to sources and also a few likely reason codes.  also change the primary key of the reason table to not autoincrement since we'll be hand-inserting all the rows of this table and we don't want to have conflicting IDs.  this resolves issue #91

---
+++ src/db/migrations/20120924030629-add-not-indexable-reason-to-sources.js
@@ -0,0 +1,46 @@
+var dbm = require('db-migrate');
+var type = dbm.dataType;
+var Step = require('step');
+
+exports.up = function(db, callback) {
+  Step(
+    function () {
+      var group = this.group();
+      db.addColumn("sources", "not_indexable_reason_id", "int", group());
+      db.changeColumn("reasons", "id", { primaryKey: true, autoIncrement: false, notNull: true }, group()); // drop autoincrement from PK because we'll be inserting records manually
+      db.insert("reasons", ["id", "title", "description"], [1, "Resource Unavailable", "The requested URL is unavailable due to server down or HTTP 4xx errors"], group());
+      db.insert("reasons", ["id", "title", "description"], [2, "Not a Feed", "The URL is accessible, but it is not a parseable feed"], group());
+      db.insert("reasons", ["id", "title", "description"], [3, "No Unique Content", "The feed is a subset or duplicate of another feed in the database, so it does not offer any unique content"], group());
+      db.insert("reasons", ["id", "title", "description"], [4, "Garbage", "Content is spam or fake or other crap we do not want to waste our time on"], group());
+      db.insert("reasons", ["id", "title", "description"], [5, "Illegal", "We are required by law to block this content or it is believed to cause immense liability if we do not"], group());
+      db.insert("reasons", ["id", "title", "description"], [6, "Alternate Content Source", "The content of this feed is acquired through some other method (such as direct API calls)"], group());
+    },
+    // use SQL until db-migrate supports adding constraints natively
+    function constrain(err) {
+      if (err) { callback(err); return; }
+      db.runSql(
+        'ALTER TABLE sources ADD FOREIGN KEY (not_indexable_reason_id) REFERENCES reasons;'
+        , this);
+    },
+    callback
+  );
+};
+
+exports.down = function(db, callback) {
+  Step(
+    function () {
+      var group = this.group();
+      db.removeColumn("sources", "not_indexable_reason_id", group());
+      db.changeColumn("reasons", "id", { primaryKey: true, notNull: true, autoIncrement: true }, group());
+      db.runSql("DELETE FROM reasons", group());
+    },
+    /* use SQL until db-migrate supports adding constraints natively
+    function constrain(err) {
+      if (err) { callback(err); return; }
+      db.runSql(
+        'ALTER TABLE sources DROP CONSTRAINT sources_not_indexable_reason_id_fkey'
+        , this);
+    },*/
+    callback
+  );
+};
