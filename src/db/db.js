8919441ed69aaffcc1d22ec4731e176f89a2a558
Shane Gibbons <shane.r.gibbons@gmail.com>
2012-10-08 06:20:45 +0800

    proper uri escaping in connection string for db helper for migrate

--- src/db/db.js
+++ src/db/db.js
@@ -1,7 +1,7 @@
 var pg = require('pg');
 var config = require('./database.json')[process.env.LEDE_DB];
 
-var conn_string = 'tcp://' + config.user + ':' + config.password + '@' + config.host + '/' + config.database;
+var conn_string = 'tcp://' + encodeURIComponent(config.user) + ':' + encodeURIComponent(config.password) + '@' + encodeURIComponent(config.host) + '/' + encodeURIComponent(config.database);
 exports.run = function(query, cb) {
   pg.connect(conn_string, function(err, client) {
     if(err) {
