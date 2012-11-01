edda6e887b3a462a28b8d37486e4fd07d003dbe4
Justin Fyles <tullfan@gmail.com>
2012-10-31 23:51:49 -0400

    basic outline for how the recommender will work.  next step? get that shit in real code

--- src/recommender/recommender.js
+++ src/recommender/recommender.js
@@ -8,19 +8,22 @@ function generateDailyEmails(numberOfLedes) {
 	// Get all users
 
 	// For each unique user:
-	// <async> fetchLedesForUser (callback: fetchLedesforUser)
+	// <async> fetchLedesForUser
 
 } 
 
-function fetchLedesForUser(user, resolveLedesCallback) {
+function fetchLedesForUser(user) {
   // For the given user, get all the ledes from the database (callback: resolveLedesToPosts)
 }
 
-function resolveLedesToPosts(ledes, getTopRelatedPosts) {
-  // 
+function resolveLedesToPosts(ledes) {
+  // Check posts database for lede url (callback: getTopRelatedPosts)
 }
 
-
+function getTopRelatedPosts(post) {
+  // Step over this for each top related post and get all the results into an array. Flatten the array, and take the top 5 highest ledefactor posts.
+  ledefactor.getTopRelatedPosts(post, parallel());
+}
 
 // Handle top-level exceptions
 process.on('uncaughtException',function(error) {
@@ -32,4 +35,3 @@ process.on('uncaughtException',function(error) {
     process.exit(1);
   }, 10000);
 });
-
