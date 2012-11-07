7e8de3c05cb267936fe70ed617cab354f709dae4
Shane Gibbons <shane.r.gibbons@gmail.com>
2012-11-06 19:52:46 -0500

    logging for generic app server error handler

--- src/web/app/helpers/core.js
+++ src/web/app/helpers/core.js
@@ -1,7 +1,9 @@
+var log = require('../../../core/logger').getLogger("web");
 
 // emit a standard 500 server error response given an error object
 function generic_error(res, err) {
   // TODO: better error message, status code
+  log.error('Server Error: ' + err);
   res.status(500); // generic server fail
   res.send({ result: 'Server Error: ' + err });
 }
