a177d47fcce1c739afda47868717bd38f93d0eb1
Justin Fyles <tullfan@gmail.com>
2012-11-14 23:29:06 -0500

    added an email for if no ledes are found

--- src/notifier/notifier.js
+++ src/notifier/notifier.js
@@ -8,7 +8,7 @@ var nodemailer = require('nodemailer');
 var dataLayer = require('../core/datalayer');
 var errors = require('../core/errors.js');
 
-//resolve a user id to a user object
+// Resolve a user id to a user object
 function get_user(userid, cb) {
   dataLayer.User.findOne(userid, {only: ['id','email']}, function(err, user) {
     if(err) {
@@ -19,7 +19,7 @@ function get_user(userid, cb) {
   });
 }
 
-//resolve an array of post ids to an array of post objects (culled for id,uri,title)
+// Resolve an array of post ids to an array of post objects (culled for id,uri,title)
 function get_posts(postids, cb){
   if(_.isEmpty(postids)) {
     log.error("Attempted to resolve empty list of post IDs");
@@ -34,12 +34,8 @@ function get_posts(postids, cb){
   });
 }
 
-//use the user and posts informtion to generate the content of the email
-function generate_daily_options (user,posts) {
-  var source = fs.readFileSync(__dirname + '/views/notification.hjs', 'utf8');
-  var template = handlebars.compile(source);
-  var mail_html = template({ledes: posts});
-
+// Send the pre formatted daily email
+function send_daily_email(user, mail_html, callback) {
   var mail_options = {
     from: "hello@unburythelede.com",
     to: user.email,
@@ -47,11 +43,46 @@ function generate_daily_options (user,posts) {
     html: mail_html
   };
 
-  return mail_options;
+  send_email(mail_options, callback);
 }
 
-//use the user and posts informtion to generate the content of the email
-function generate_welcome_options (user,temp_password) {
+// Send the daily email formatted for use with ledes
+function generate_daily_email_with_ledes(user, posts, callback) {
+  var source = fs.readFileSync(__dirname + '/views/daily.hjs', 'utf8');
+  var template = handlebars.compile(source);
+  var mail_html = template({ledes: posts});
+
+  send_daily_email(user, mail_html, callback);
+}
+
+// Send the daily email formatted for use without ledes
+function generate_daily_email_without_ledes(user, callback) {
+  var source = fs.readFileSync(__dirname + '/views/daily_no_ledes.hjs', 'utf8');
+  var template = handlebars.compile(source);
+
+  var links = 
+  [
+    {"uri": "http://cat-blog.tumblr.com/", "title": "A totally normal blog"},
+    {"uri": "http://www.theonion.com/", "title": "Some super serious world news"},
+    {"uri": "http://somerville.patch.com/", "title": "News from the Lede stomping ground"}
+  ]
+
+  var mail_html = template({suggestions: links});
+
+  send_daily_email(user, mail.html, callback);
+}
+
+// Use the user and posts information to generate the content of the daily email and send it
+function generate_and_send_daily_email (user, posts, callback) {
+  if(posts && posts.length > 0) {
+    generate_daily_email_with_ledes(user, posts, callback);
+  } 
+
+  generate_daily_email_without_ledes(user, callback);
+}
+
+// Use the user and password informtion to generate a welcome email and send it
+function send_welcome_email (user, temp_password, callback) {
   var source = fs.readFileSync(__dirname + '/views/welcome.hjs', 'utf8');
   var template = handlebars.compile(source);
   var mail_html = template({password: temp_password, username: user.email});
@@ -63,12 +94,14 @@ function generate_welcome_options (user,temp_password) {
     html: mail_html
   };
 
-  return mail_options;
+  send_email(mail_options, callback);
 }
 
-//actually call the mailer to hit sendgrid
+// Sent the constructed email to the customer through the email service provider
 function send_email(mailOptions, callback) {
-  // set up mailer
+  log.debug(mail_options);
+
+  // Set up mailer
   var smtpTransport = nodemailer.createTransport("SMTP", {
     service: settings.notifier.service,
     auth: {
@@ -89,25 +122,21 @@ function send_email(mailOptions, callback) {
   smtpTransport.close();
 }
 
-// wrap it all up.  resolve userid and postids, generate email and send it
+// Wrap it all up. Resolve userid and postids, generate email, and send it
 exports.send_daily = function(userid, postids, callback) {
   get_user(userid, function(user) {
     log.debug('Found user ' + user.email);
     get_posts(postids, function(posts) {
       log.debug('Found posts');
-      var mail_options = generate_daily_options(user,posts);
-      log.debug(mail_options);
-      send_email(mail_options, callback);
+      generate_and_send_daily_email(user, posts, callback);
     });
   });
 };
 
-// wrap it all up.  resolve userid, generate email and send it
+// Wrap it all up. Resolve userid, generate email, and send it
 exports.send_welcome = function(userid, temp_password, callback) {
   get_user(userid, function(user) {
     log.debug('Found user ' + user.email);
-    var mail_options = generate_welcome_options(user,temp_password);
-    log.debug(mail_options);
-    send_email(mail_options, callback);
+    send_welcome_email(mail_options, callback);
   });
 };
