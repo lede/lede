01fedc174761004edd85a2b6a981173afddfbc4a
Justin Fyles <tullfan@gmail.com>
2012-11-19 23:30:50 -0500

    streamlining the run query

--- src/core/graph-database-test.js
+++ src/core/graph-database-test.js
@@ -1,22 +1,15 @@
 settings = require('./settings').get("indexer");
 var graphDatalayer = require('./graph-datalayer');
 
-graphDatalayer.getClient(function(err, db) {
-  if(err){
+var query = "SELECT FROM OGraphVertex";
+
+graphDatalayer.query(query, function(error, results) {
+  if(error){
     console.log(err);
   }
 
-  console.log("Running select...");
-  db.command("SELECT FROM OGraphVertex", function(error, results) {
-    if(error){
-      console.log(err);
-    }
-
-    console.log("Found results:");
-    for (var i in results) {
-      console.log(results[i]);
-    }
-
-    db.close();
-  });
+  console.log("Found results:");
+  for (var i in results) {
+    console.log(results[i]);
+  }
 });
