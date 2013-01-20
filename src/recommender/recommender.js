8e341dcfad34ff2c88ab8710e2e6c337f7af5729
Ryan Meador <ryan.d.meador@gmail.com>
2013-01-20 18:40:48 -0500

    recommender now fetches all ledes for all users and sends the emails

--- src/recommender/recommender.js
+++ src/recommender/recommender.js
@@ -2,77 +2,51 @@ settings = require('../core/settings').get("recommender");
 log = require('../core/logger').getLogger("recommender");
 var User = require('../core/datalayer').User;
 var _ = require('underscore');
-var orm = require('../core/datalayer').client;
+var dataLayer = require('../core/datalayer');
 var notifier = require('../notifier/notifier');
 var util = require('util');
-
-//n-deep backlink tracking currently produces a list of from_uris
-//we treat encountering the same link row (id) as a loop and stop recursing
-//the path array holds a list of link IDs that represent the path traversed to get from from_uri to uri
-function backlinksQuery(userId, limit) {
-
-  return "WITH RECURSIVE "+
-    "search_links(id, uri, from_uri, link_text, created_at, updated_at, depth, path, cycle) AS "+ 
-    "( SELECT "+
-      "l.id, l.uri, l.from_uri, l.link_text, l.created_at, l.updated_at, 1, ARRAY[l.id], false "+
-      "FROM links l "+
-      "UNION ALL "+
-      "SELECT "+
-        "l.id, l.uri, l.from_uri, l.link_text, l.created_at, l.updated_at, sl.depth + 1, "+
-        "path || l.id, l.id = ANY(path) "+
-        "FROM links l, search_links sl "+
-        "WHERE lower(l.uri) = lower(sl.from_uri) AND NOT cycle AND lower(l.uri) != lower(l.from_uri)"+
-    ") "+
-    "SELECT sl.from_uri as uri, COALESCE(p.title, sl.from_uri) as title "+
-    "FROM search_links sl "+
-    "LEFT JOIN posts p ON lower(sl.from_uri) = lower(p.uri) "+
-    "JOIN ledes le ON lower(sl.uri) = lower(le.uri) "+
-    "WHERE le.user_id = " + userId + " AND (sl.created_at > now() - interval '1 day') "+
-    "ORDER BY sl.created_at DESC LIMIT "+ limit;
-
-}
+var Step = require('step');
 
 function generateDailyEmails(numberOfLedes, done) {
 	// Get all users
   log.info('Starting email generation process for all users...');
 
-  orm.emit("query", "SELECT id FROM users", function(err, result) {
+  dataLayer.User.find({}, { only: ['id', 'email'] }, function(err, users) {
     if(err) {
       log.error('Error getting user ids: ' + err);
+      done(err);
     } else {
-      log.info('Got result from user id lookup, found : ' + util.inspect(result.rows));
-      // HACK: use step or something similar here - also note this wouldn't be necessary at all if we weren't getting magically daemonized by some unseen evil in the settings stuff
-      var outstanding = result.rows.length;
-      _.each(
-        _.pluck(result.rows, 'id'),
-        function(userId) { 
-          fetchLedesForUser(userId, numberOfLedes, function() {
-            log.info('Completed ledes for ' + userId);
-            outstanding--;
-            if(outstanding === 0) {
-              done();
+      log.debug('Got result from user id lookup, found : ' + util.inspect(users));
+      Step(
+        function () {
+          var group = this.group();
+
+          _.each(
+            users,
+            function (user) { 
+              var cb = group();
+
+              fetchLedesForUser(user.id, numberOfLedes, function(err, ledes) {
+                if(err) {
+                  log.error('Error finding Ledes for user ' + user.id + ' : ' + err);
+                  cb(err);
+                } else {
+                  log.info('Sending email to user ' + user.id + ' (' + user.email + ') with ' + ledes.length + ' links');
+                  notifier.send_daily(user, ledes, cb);
+                }
+              });
             }
-          });
-        }
+          );
+        },
+        done
       );
-      if(outstanding === 0) { done(); }
     }
   });
 } 
 
 function fetchLedesForUser(userId, limit, done) {
-
-  log.info('Fetching ledes for user: ' + userId);
-  log.debug('query ' + backlinksQuery(userId, limit));
-  orm.emit('query', backlinksQuery(userId, limit), function(err, result) {
-    if(err) {
-      log.error('Error finding backlinks for user: ' + userId + ' : ' + err);
-      done();
-    } else {
-      log.info('Sending email to ' + userId + ' with : ' + result.rows.length + ' links');
-      notifier.send_daily(userId, result.rows, done);
-    }
-  });
+  log.debug('Fetching ledes for user: ' + userId);
+  dataLayer.Recommendation.find({ user_id: userId, sent: false }, { order: ['created_at'], limit: limit }, done);
 }
 
 // Handle top-level exceptions
