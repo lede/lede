98c96cfb351e5225f5619912bfdc0dcf84599cb3
Justin Fyles <tullfan@gmail.com>
2013-01-30 21:27:41 -0500

    Finally got the migration in tip top shape

--- src/db/migrations/025-add_email_tracking_columns.js
+++ src/db/migrations/025-add_email_tracking_columns.js
@@ -2,6 +2,7 @@ var db = require('../db');
 
 exports.up = function(next){
   db.run("ALTER TABLE notifications ADD COLUMN delivered_at timestamp", function(err, result) {
+    console.log(err);
     db.run("ALTER TABLE notifications ADD COLUMN opened_at timestamp", next);
   });
 };
