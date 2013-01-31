ea01e75dd2ca001ca65b642c3e32038485df3995
Justin Fyles <tullfan@gmail.com>
2013-01-30 20:47:51 -0500

    huge typo in the migration

--- src/db/migrations/025-add_email_tracking_columns.js
+++ src/db/migrations/025-add_email_tracking_columns.js
@@ -8,9 +8,9 @@ exports.up = function(next){
 };
 
 exports.down = function(next){
-  db.run("ALTER TABLE notifications ADD COLUMN delivered_at timestamp", function(err, result){
-    db.run("ALTER TABLE notifications ADD COLUMN opened_at timestamp", function(err, result){
-      db.run("ALTER TABLE recommendations ADD COLUMN clicked_at timestamp", next);
+  db.run("ALTER TABLE notifications DROP COLUMN delivered_at", function(err, result){
+    db.run("ALTER TABLE notifications DROP COLUMN opened_at", function(err, result){
+      db.run("ALTER TABLE recommendations DROP COLUMN clicked_at", next);
     });
   });
 };
