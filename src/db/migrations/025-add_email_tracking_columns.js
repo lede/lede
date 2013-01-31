72e752a17eceb4021d7792ffdcc94f5698eb099f
Justin Fyles <tullfan@gmail.com>
2013-01-30 21:22:00 -0500

    breaking that shit out into 2 calls

--- src/db/migrations/025-add_email_tracking_columns.js
+++ src/db/migrations/025-add_email_tracking_columns.js
@@ -1,16 +1,13 @@
+var db = require('../db');
 
 exports.up = function(next){
-  db.run("ALTER TABLE notifications ADD COLUMN delivered_at timestamp", function(err, result){
-    db.run("ALTER TABLE notifications ADD COLUMN opened_at timestamp", function(err, result){
-      db.run("ALTER TABLE recommendations ADD COLUMN clicked_at timestamp", next);
-    });
+  db.run("ALTER TABLE notifications ADD COLUMN delivered_at timestamp", function(err, result) {
+    db.run("ALTER TABLE notifications ADD COLUMN opened_at timestamp", next);
   });
 };
 
 exports.down = function(next){
-  db.run("ALTER TABLE notifications DROP COLUMN delivered_at", function(err, result){
-    db.run("ALTER TABLE notifications DROP COLUMN opened_at", function(err, result){
-      db.run("ALTER TABLE recommendations DROP COLUMN clicked_at", next);
-    });
+  db.run("ALTER TABLE notifications DROP COLUMN delivered_at", function(err, result) {
+    db.run("ALTER TABLE notifications DROP COLUMN opened_at", next);
   });
 };
