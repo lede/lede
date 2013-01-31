e8b47f8e8a7b714abc2075c5a78a7a660d0e8bf1
Ryan Meador <ryan.d.meador@gmail.com>
2013-01-30 22:38:03 -0500

    create a tool to do stupidly simple template substitution because apparently there is no widely-available tool that does such a thing.  the best I was able to find was written in JS and installed via Ruby gem, which seemed stupid.  this was like 15 mins and 26 lines of python

---
+++ src/bookmarklet/subst.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+import sys
+
+if len(sys.argv) != 5:
+  sys.stderr.write("Need 4 arguments")
+  exit(1)
+
+filename = sys.argv[1]
+variable = sys.argv[2]
+subsitutitionType = sys.argv[3]
+substitutionValue = sys.argv[4]
+
+with open(filename, "r") as inputFile:
+  data = inputFile.read()
+
+if subsitutitionType == "--value":
+  value = substitutionValue
+elif subsitutitionType == "--file":
+  with open(substitutionValue, "r") as valueFile:
+    value = valueFile.read()
+else:
+  sys.stderr.write("Need --value or --file")
+  exit(1)
+  
+sys.stdout.write(data.replace("{{{" + variable + "}}}", value))
