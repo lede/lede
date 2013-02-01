d3ec47a153f827824495131efb528a463382f34c
jhayden <jonathan.hayden@gmail.com>
2013-01-31 22:56:05 -0500

    adding log of the inserted notification for debugging the sendgrid callbacks

--- src/notifier/notifier.js
+++ src/notifier/notifier.js
@@ -106,7 +106,7 @@ function send_email(user, mail_options, callback) {
       log.error('Failed to create record in the notifications table');
       callback(err);
     } else if (inserted_notification) {
-      log.info('Created record of notification ' + inserted_notification);
+      log.info('Created record of notification ' + util.inspect(inserted_notification));
       mail_options.headers = {"X-SMTPAPI": {"unique_args": {"notification_id": inserted_notification.id}}};
       smtpTransport.sendMail(mail_options, function(err, inserted_notification) {
         if(err) {
