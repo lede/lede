4a32e6b43cf590892970420c68254e9933f1ad21
Ryan Meador <ryan.d.meador@gmail.com>
2013-02-06 20:48:06 -0500

    do something with the response from the server, namely add it to the DOM of the pop down widget.  we can worry about styling it later (it doesn't even display for me right now, but I can see it in firebug)

--- src/bookmarklet/widget/bookmarklet.js
+++ src/bookmarklet/widget/bookmarklet.js
@@ -2,9 +2,16 @@ var yarn = require('./build/yarn');
 
 var elem = yarn('bookmarklet.html', ['bookmarklet.css']);
 
-exports.inject = function(target_element) {
-  target_element.appendChild(elem);
-  __LEDE_POPDOWN__ = elem;
-};
+exports.inject = function inject(target_element) {
+  if (typeof(response) == 'undefined' || !response) {
+    setTimeout(function() { inject(target_element); }, 100);
+  } else {
+    if (response.message) {
+      elem.querySelector('#message').textContent = response.message;
+    }
 
+    target_element.appendChild(elem);
+    __LEDE_POPDOWN__ = elem;
+  }
+};
 
