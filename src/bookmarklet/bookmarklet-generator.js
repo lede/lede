4252e8d40fee3c42fe6d17b0155bcd27a80f98f2
Ryan Meador <ryan.d.meador@gmail.com>
2013-02-10 15:54:53 -0500

    index page now includes a real bookmarklet, complete with API key.  also modified bookmarklet generator to have an ID and a parameter for inner content

--- src/bookmarklet/bookmarklet-generator.js
+++ src/bookmarklet/bookmarklet-generator.js
@@ -1,9 +1,10 @@
-function createBookmarkletLink(apikey) {
+function createBookmarkletLink(apikey, label) {
 
   var elem = document.createElement('A');
-  $(elem).html('Lede This');
+  $(elem).html(label || 'Lede This');
+  $(elem).attr("id", "install-bookmarklet");
   $(elem).addClass("bookmarklet");
-  elem.href = "javascript:(function(){var js_loader = function(source) {var script = document.createElement('SCRIPT');script.type = 'text/javascript';script.src = source;document.getElementsByTagName('head')[0].appendChild(script);}; js_loader('https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js');js_loader('http://{{{SERVER_ADDRESS}}}/bookmarklet/bundle.js');setTimeout(function() { var query = '?target=' + encodeURIComponent(window.location.href) + '&title=' + encodeURIComponent(document.title) + '&apikey=" + apikey + "&no_cache=' + Math.random(); js_loader('http://{{{SERVER_ADDRESS}}}/api/lede' + query); }, 500); })();";
+  $(elem).attr("href", "javascript:(function(){var js_loader = function(source) {var script = document.createElement('SCRIPT');script.type = 'text/javascript';script.src = source;document.getElementsByTagName('head')[0].appendChild(script);}; js_loader('https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js');js_loader('http://{{{SERVER_ADDRESS}}}/bookmarklet/bundle.js');setTimeout(function() { var query = '?target=' + encodeURIComponent(window.location.href) + '&title=' + encodeURIComponent(document.title) + '&apikey=" + apikey + "&no_cache=' + Math.random(); js_loader('http://{{{SERVER_ADDRESS}}}/api/lede' + query); }, 500); })();");
 
   return elem;
 }
