b9c843f8668bdae40ee15143a1282061a266ad81
Justin Fyles <tullfan@gmail.com>
2013-02-28 05:10:15 -0500

    Lint

--- src/db/settings_helper.js
+++ src/db/settings_helper.js
@@ -1,4 +1,4 @@
-if (process.argv.length < 3 || process.argv[2] == "") {
+if (process.argv.length < 3 || process.argv[2] === "") {
   console.log("Settings file must be first parameter");
   process.exit(1);
 }
@@ -9,7 +9,7 @@ var settings_file = path.basename(process.argv[2], '.js');
 var prefix = (settings_file.split('-'))[0];
 var username = (settings_file.split('-'))[1];
 
-if (prefix != 'settings' || username == '') {
+if (prefix != 'settings' || username === '') {
   console.log("Settings filename must be of the form 'settings-<username>.js'");
   process.exit(1);
 }
@@ -26,6 +26,6 @@ dbconf[settings.dbConnectionParams.user] = {
 }
 
 var buffer = JSON.stringify(dbconf);
-fs.writeFileSync('TESTdatabase.json', buffer); 
+fs.writeFileSync('TESTdatabase.json', buffer);
 
 console.log(dbconf);
