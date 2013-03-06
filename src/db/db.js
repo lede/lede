90f9d782e179b8cf40540ff8331856c1e562a7fe
Ryan Meador <ryan.d.meador@gmail.com>
2013-03-06 18:31:49 -0500

    checkpoint recommendations->stories migration

--- src/db/db.js
+++ src/db/db.js
@@ -2,18 +2,24 @@ var pg = require('pg');
 var config = require('./database.json')[process.env.LEDE_DB];
 
 var conn_string = 'tcp://' + encodeURIComponent(config.user) + ':' + encodeURIComponent(config.password) + '@' + encodeURIComponent(config.host) + '/' + encodeURIComponent(config.database);
-exports.run = function(query, cb) {
+exports.run = function(query, vars, cb) {
   pg.connect(conn_string, function(err, client) {
     if(err) {
       console.err(err);
       cb(err);
     } else {
-      client.query(query, function(err, res) {
+      if (!cb) { // compensate for only having 2 arguments, which should be query and cb
+        cb = vars;
+        vars = [];
+      }
+
+      client.query(query, vars, function(err, res) {
         if(err) {
           console.log(err);
         }
-        cb();
+        cb(err, res);
       });
+    }
     }
   });
 };
---
+++ src/db/migrations/033-migrate_ledes_and_recommendations.js
@@ -0,0 +1,47 @@
+var db = require('../db');
+var _ = require('../underscore');
+var Step = require('../underscore');
+
+exports.up = function(next){
+  db.run("SELECT DISTINCT uri FROM recommendations", function(err, result) {
+    if (err) {
+      console.log(err);
+      next();
+    } else {
+      Step(
+        function() {
+          var group = this.group();
+
+          _.each(result.rows, function(row) {
+            db.run("SELECT * FROM Recommendations WHERE uri = $1 ORDER BY created_at ASC LIMIT 1", [row.uri], group());
+          });
+        },
+        function(err, results) {
+          if (err) {
+            throw err;
+          }
+
+          var group = this.group();
+
+          _.each(results, function(story) {
+            db.run("INSERT INTO Stories (uri, title, description, author, image_url, origin_type, created_by_user_id, created_at, updated_at) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9", [], group());
+          });
+        },
+        next
+      );
+    }
+  });
+};
+
+exports.down = function(next){
+  db.run("DELETE FROM Stories", function(err, result) {
+    if(err) {
+      console.log(err);
+      next();
+    } else {
+      //db.run("DROP SEQUENCE apikeys_id_seq", next);
+      next();
+    }
+  });
+};
+
