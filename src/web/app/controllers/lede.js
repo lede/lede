f5fc7a7d373c5e58b2fa3c9a808e6fd9065ef414
Ryan Meador <ryan.d.meador@gmail.com>
2013-03-09 19:12:35 -0500

    pretty extensive rework of what happens when you click the bookmarklet.  the data is now fed through canonicalization and extraction, then put into the new normalized Story data table, and finally linked to Lede.  there is a known issue in that it tries to create the Story every time you click the bookmarklet, even if it already exists, which I will fix presently but this needs to be checkpointed

--- src/web/app/controllers/lede.js
+++ src/web/app/controllers/lede.js
@@ -1,5 +1,4 @@
-var Lede = require('../../../core/datalayer').Lede;
-var Post = require('../../../core/datalayer').Post;
+var dataLayer = require('../../../core/datalayer');
 var _ = require('underscore');
 var util = require('util');
 var no_err = require('../helpers/core').no_err;
@@ -8,30 +7,60 @@ var queues = require('../../../core/resque-queues');
 var query = require('./query');
 var url = require('url');
 var User = require('../../../core/datalayer').User;
+var canonicalize = require('../../../core/canonicalize').canonicalize;
+var extractor = require('../../../indexer/extractor');
 
 function createLede(req, res) {
   // clean this up, but:
   // ensure we have a valid request
   if(!req.query.target) {
     // invalid request, say so:
-    log.warn('Malformed bookmarklet request: ' + util.inspect(req.query));
+    log.warn("Malformed bookmarklet request from user " + req.user.id + ": " + util.inspect(req.query));
     res.send("var response = { success: false, message: 'Not all required request fields were present.' };");
     return;
   }
 
-  log.info('Bookmarklet request looks ok, processing...');
+  // return the response to the user immediately, because our processing will
+  // have noticeable lag (multiple requests to foreign servers)
+  log.info("Bookmarklet request from user " + req.user.id + " for '" + req.query.target + "'");
+  res.send("var response = { success: true };");
 
-  // don't pass parent id since there's no meaning to it here and discoverer seems to ignore it anyway
-  queues.fastDiscover.enqueue({ parentId: null, url: req.query.target});
+  canonicalize(req.query.target, function (err, canonicalUrl) {
+    // attempt some discovery (don't pass parent id since there's no meaning to
+    // it here and discoverer seems to ignore it anyway)
+    // TODO we probably want to wrap this in a function that checks to see if
+    // we've already performed discovery on this URL recently
+    queues.fastDiscover.enqueue({ parentId: null, url: canonicalUrl });
 
-  // create a Lede that points at the post we either created or found
-  Lede.create({ uri: req.query.target, title: req.query.title, user_id: req.user.id }, no_err(res, function(results) {
-
-    // let the client know we're done here.
-    log.info('Created a new Lede with ID: ' + results.rows[0].id);
-    res.send("var response = { success: true };");
-
-  }));
+    // extract that bitch
+    extractor.extractContent(canonicalUrl, function (err, content) {
+      if (err) {
+        log.error("While extracting bookmarklet URL '" + canonicalUrl + "' for user " + req.user.id + ": " + err);
+      } else {
+        dataLayer.Story.create({uri: canonicalUrl,
+          title: content.title,
+          description: content.description,
+          author: content.author,
+          image_url: content.image,
+          created_by_user_id: req.user.id,
+          origin_type: 1
+        }, 
+        function (err, storyResult) {
+          if (err) {
+            log.error("While creating story from bookmarklet URL '" + canonicalUrl + "' for user " + req.user.id + ": " + err);
+          } else {
+            dataLayer.Lede.create({ user_id: req.user.id, story_id: storyResult.rows[0].id }, function (err, ledeResult) {
+              if (err) {
+                log.error("While creating Lede to story " + storyResult.rows[0].id + " for user " + req.user.id + ": " + err);
+              } else {
+                log.info("Finished processing Lede " + ledeResult.rows[0].id + " for user " + req.user.id);
+              }
+            });
+          }
+        });
+      }
+    });
+  });
 }
 
 exports.create = function(req, res) {
@@ -40,7 +69,7 @@ exports.create = function(req, res) {
 
 exports.list = function(req, res) {
   var tq = query.translate(req.query);
-  Lede.find(tq.select, tq.attributes, no_err(res, function(results) {
+  dataLayer.Lede.find(tq.select, tq.attributes, no_err(res, function(results) {
     log.info("Listed Ledes for user " + req.user.id);
     res.send(results);
   }));
