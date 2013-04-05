d810f13145a0d44c4a00c555dc7eaaaf8db716cb
Ryan Meador <ryan.d.meador@gmail.com>
2013-04-04 21:57:48 -0400

    checkpoint of rewrite of feed fetching and parsing using new libraries

--- src/indexer/feedprocessor.js
+++ src/indexer/feedprocessor.js
@@ -1,11 +1,47 @@
 var FeedParser = require('feedparser');
+var request = require('request');
 var dataLayer = require('../core/datalayer');
 var linkTracker = require('./linktracker');
 var _ = require('underscore');
 var util = require('util');
 var graphDatalayer = require('../core/graph-datalayer');
+var Step = require('step');
 
 // NOTE: Throughout this file, 'post' refers to the post in the database, and 'article' refers to the article parsed from the external feed
+
+function parseFeed(source, done) {
+  Step(
+    function() {
+      var group = this.group();
+
+      var endEvent = group(); // create one event up front so we guarantee it doesn't advance to the next step before the end event fires
+      
+      request(source.url)
+        .pipe(new FeedParser())
+        .on('error', function(error) {
+          done(error); // TODO wrap error in a descriptive object
+        })
+        .on('meta', function (meta) {
+          // we process meta here instead of in the 'complete' event handler so
+          // we can spread out the load, not that it's likely to actually
+          // matter
+          updateFeedMetadata(source, meta, group());
+        })
+        .on('complete', function (meta, articles) {
+          createOrUpdateStories(source, articles, group());
+        })
+        .on('end', function() {
+          log.trace("parsing complete");
+          endEvent();
+        });
+    },
+    function(err, results) {
+    }
+  );
+}
+
+function updateFeedMetadata(source, meta, done) {
+}
 
 function parseFeed(source, xml, done) {
   try {
